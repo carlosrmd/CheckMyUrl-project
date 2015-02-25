bfff5ed6cdf53c42b24e086e9c006fec57d34260
Carlos <carlosrmd94@gmail.com>
2015-02-25 09:26:00 -0430

    Agregue el form y el view

--- urlchecker/views.py
+++ urlchecker/views.py
@@ -7,3 +7,21 @@ def index(request):
     t = loader.get_template('index.html')
     c = Context({'foo': 'bar'})
     return HttpResponse(t.render(c))
+
+def checking(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = UrlForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'index.html', {'form': form})
